76128ffe06482cd0df8c358b955f7cc35ad09599
zwahab <zainabwahab.eth@gmail.com>
2026-03-26 18:08:50 +0100

    working with web socket

--- log-aggregator/main.go
+++ log-aggregator/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net/http"
 	"os"
+	"strings"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 type LogEntry struct {
@@ -34,11 +38,46 @@ func watchFile(filename string, lines chan<- string) {
 
 }
 
+func parseLogLine(line string) (LogEntry, error) {
+	s := strings.SplitN(line, " ", 4)
+
+	if len(s) != 4 {
+		return LogEntry{}, fmt.Errorf("invalid log line: %s", line)
+	}
+
+	return LogEntry{
+		Level:     s[2],
+		Message:   s[3],
+		Timestamp: s[0] + " " + s[1],
+	}, nil
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	conn.WriteJSON()
+}
+
 func main() {
 	lines := make(chan string)
 	go watchFile("operation/app.log", lines)
 
 	for line := range lines {
-		fmt.Println(line)
+		entry, err := parseLogLine(line)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(entry)
 	}
+
 }
